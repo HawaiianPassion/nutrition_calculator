0f0363929cc3739cb66c74ea8c8ea296c66321b5
HawaiianPassion <rysongit@gmail.com>
2015-04-11 19:26:44 -0700

    added new scene for results.  All views are properly constrained.  No functionality yet...

---
+++ Nutrition Calculator/Nutrition Calculator/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  Nutrition Calculator
+//
+//  Created by Ryson Asuncion on 4/11/15.
+//  Copyright (c) 2015 Derp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
